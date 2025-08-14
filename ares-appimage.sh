77d054226e7bd1cf0fcf7c616b4a360322c4d11d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-13 23:51:49 -0400

    fix `aarch64` failing

--- ares-appimage.sh
+++ ares-appimage.sh
@@ -8,12 +8,12 @@ GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk
 # Determine to build nightly or stable
 if [ "$1" = 'devel' ]; then
 	echo "Making nightly build of ares..."
-	UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|nightly|*$ARCH.AppImage.zsync"
+	export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|nightly|*$ARCH.AppImage.zsync"
 	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9)"
 	git clone "$REPO"
 else
 	echo "Making stable build of ares..."
-	UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+	export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 	wget "$GRON" -O ./gron.awk
 	chmod +x ./gron.awk
 	VERSION=$(wget https://api.github.com/repos/ares-emulator/ares/tags -O - \
@@ -52,18 +52,21 @@ URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
 export ADD_HOOKS="self-updater.bg.hook"
-export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export OUTNAME=ares-"$VERSION"-anylinux-"$ARCH".AppImage
 export DESKTOP=/usr/share/applications/ares.desktop
 export ICON=/usr/share/icons/hicolor/256x256/apps/ares.png
 export DEPLOY_OPENGL=1 
 export DEPLOY_PIPEWIRE=1
 
+# "fix" xvfb-run failing to kill the process in aarch64
+if [ "$ARCH" = "aarch64" ]; then
+	sed -i 's#kill $XVFBPID#kill $XVFBPID || true#' "$(command -v xvfb-run)"
+fi
+
 # ADD LIBRARIES
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/ares /usr/bin/sourcery
-cp -rv /usr/share/ares  ./AppDir/share
 
 # turn appdir into appimage
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
