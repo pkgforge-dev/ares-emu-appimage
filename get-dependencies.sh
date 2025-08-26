2dcf3e8007af406d42c3e9ae019de34e06e7e622
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-26 11:08:22 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -39,7 +39,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl gtk3-mini opus-mini libxml2-mini
+./get-debloated-pkgs.sh --add-common
 
 # Make librashader
 echo "Making extra dependencies..."
